0c81e3a3933673e4e67b5d96624ee357e45970cc
Bibash Chaudhary <bibashcdry46@gmail.com>
2024-11-10 23:51:22 +0545

    Update ordersApiSlice.js

--- frontend/src/slices/ordersApiSlice.js
+++ frontend/src/slices/ordersApiSlice.js
@@ -46,9 +46,10 @@ export const orderApiSlice = apiSlice.injectEndpoints({
       keepUnusedDataFor: 5,
     }),
     deliverOrder: builder.mutation({
-      query: (orderId) => ({
+      query: ({orderId,order) => ({
         url: `${ORDERS_URL}/${orderId}/deliver`,
         method: "PUT",
+        body:order,
       }),
     }),
   }),
